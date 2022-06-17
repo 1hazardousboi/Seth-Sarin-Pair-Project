c3ac3397a6be916b5b8e836730c1d1ecde32497e
1hazardousboi <sarinjthorn@gmail.com>
2022-06-17 14:30:36 -0600

    continued progress on pixel art JS

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 // Your code here
-let table = document.getElementsByTagName("table");
+let table = document.getElementsByTagName("table")[0];
 
 const newTr = document.createElement("tr");
 
-const tr = document.getElementsByTagName("tr");
+const tr = document.getElementsByTagName("tr")[0];
 
 const newTd = document.createElement("td");
 
+const td = document.getElementsByTagName("td")[0];
+
 function makeRow() {
   const row = document.createElement("tr");
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 8; i++) {
     const td = document.createElement("td");
     row.appendChild(td);
     // console.log("the loop is running");
   }
-  table[0].appendChild(row);
+  table.appendChild(row);
 }
 
 makeRow();
@@ -22,3 +24,25 @@ makeRow();
 const addedRow = document.getElementById("add-row");
 
 addedRow.addEventListener("click", makeRow);
+
+table.addEventListener("click", colorize);
+
+selector = document.getElementsByTagName("select")[0];
+
+let chosenColor = "red";
+
+selector.addEventListener("change", function (event) {
+  chosenColor = event.target.value;
+});
+
+function colorize(event) {
+  const target = event.target;
+  console.log("CLICKED WHOOO");
+  if (event.target.tagName === "TD") {
+    if (target.className.length) {
+      target.className = "";
+    } else {
+      target.className = chosenColor;
+    }
+  }
+}
