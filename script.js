24fbe97cbda757ec3c499ded2ffd45d57e047ffb
scatterdays <scatterdayseth@gmail.com>
2022-06-17 14:54:56 -0400

    Add a row button

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Your code here
+let table = document.getElementsByTagName("table");
+
+const newTr = document.createElement("tr");
+
+const tr = document.getElementsByTagName("tr");
+
+const newTd = document.createElement("td");
+
+function makeRow() {
+  const row = document.createElement("tr");
+  for (let i = 0; i < 3; i++) {
+    const td = document.createElement("td");
+    row.appendChild(td);
+    // console.log("the loop is running");
+  }
+  table[0].appendChild(row);
+}
+
+makeRow();
+
+const addedRow = document.getElementById("add-row");
+
+addedRow.addEventListener("click", makeRow);
